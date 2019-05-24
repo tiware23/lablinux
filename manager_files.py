0dfbf0e79b9b328e11ef61eb82273295bc3592e9
Thiago Cavalcante <thiago.cavalcante@ltmfidelidade.com.br>
2019-05-24 08:26:37 -0300

    Updated file

--- manager_files.py
+++ manager_files.py
@@ -1,12 +1,18 @@
 import os
-import zipfile
+import shutil
+import argparse
 from ftplib import FTP
 
 
-def backup_of_file():
+def print_message_backup():
     print("*" * 21)
     print("Doing backup files...")
     print("*" * 21)
+
+
+def backup_of_file(ftp):
+    print_message_backup()
+
     with open("lista.txt", "r") as deploy_list_bkp:
         for lists in deploy_list_bkp:
 
@@ -20,14 +26,18 @@ def backup_of_file():
             with open(file_backup, 'wb') as local_file:
                 ftp.retrbinary("RETR " + file_orig, local_file.write)
 
-                # with zipfile.ZipFile('backup.zip', 'w') as myzip:
-                #     myzip.write(path_backup)
+
+def zip_backup_directory():
+    shutil.make_archive("backup", 'zip', path_backup)
 
 
-def upload_file():
-    print("*" * 18)
+def print_message_upload():
     print("Uploading files...")
     print("*" * 18)
+
+
+def upload_file(ftp):
+    print_message_upload()
     with open("lista.txt", "r") as deploy_list_upload:
         for lists in deploy_list_upload:
 
@@ -39,17 +49,29 @@ def upload_file():
 
     print("Uploaded the Files")
 
+def manager_files(ftp):
+    try:
+        os.mkdir(path_backup,0o775)
+    except FileExistsError:
+        print("The local backup Directory already exists!")
+    else:
+        with FTP(args.host) as ftp:
+            ftp.login(user=args.user, passwd=args.password)
+            backup_of_file(ftp)
+            upload_file(ftp)
+            ftp.quit()
+            zip_backup_directory()
 
+
+# Backup Directory
 path_backup = "backup/"
 
+# Args to script file
+parser = argparse.ArgumentParser()
+parser.add_argument("-H", "--host", help="FTP's address")
+parser.add_argument("-u", "--user", help="FTP's user")
+parser.add_argument("-p", "--password", help="FTP's password")
+args = parser.parse_args()
 
-try:
-    os.mkdir(path_backup,0o775)
-except FileExistsError:
-    print("The local backup Directory already exists!")
-else:
-    with FTP("localhost") as ftp:
-        ftp.login(user="ftp", passwd="ftp")
-        backup_of_file()
-        upload_file()
-        ftp.quit()
+if __name__ == "__main__":
+    manager_files("ftp")
