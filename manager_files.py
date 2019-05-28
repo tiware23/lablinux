f163fa4c54ba5d9d24495b281baa01b3a43bcd00
Thiago Cavalcante <thiago.cavalcante@ltmfidelidade.com.br>
2019-05-28 10:36:36 -0300

    Updated files

--- manager_files.py
+++ manager_files.py
@@ -1,8 +1,21 @@
 import os
 import shutil
 import argparse
+import datetime
 from ftplib import FTP
 
+#############################################################################
+# This about replace the files on FTP, following the file "lista.txt",
+# which contains absolute path.
+#
+# Command Usage:
+#
+# python3 manager_files.py -H <ftp_address> -u <user> -p <pass> -C <campaign>
+#
+# This scrips is based on FTP Lib
+#
+# Author: Thiago N Cavalcante
+#############################################################################
 
 def print_message_backup():
     print("*" * 21)
@@ -16,19 +29,26 @@ def backup_of_file(ftp):
     with open("lista.txt", "r") as deploy_list_bkp:
         for lists in deploy_list_bkp:
             file_orig = lists.strip()
+
             print(file_orig)
+
             file_orig_file = file_orig.split("/")
+
             print("Backup the follow files: {}".format(file_orig_file[-1]))
             print("#" * len(file_orig_file[-1]))
             print("-" * 21)
-            file_backup = path_backup + file_orig_file[-1]
+
+            path_file = "/".join(file_orig_file[:-1])
+            os.makedirs(args.campaign + path_backup + path_file, 0o775, exist_ok=True);
+
+            file_backup = args.campaign + path_backup + file_orig
 
             with open(file_backup, 'wb') as local_file:
                 ftp.retrbinary("RETR " + file_orig, local_file.write)
 
 
 def zip_backup_directory():
-    shutil.make_archive("backup", 'zip', path_backup)
+    shutil.make_archive(args.campaign + path_backup , 'zip', args.campaign + path_backup)
 
 
 def print_message_upload():
@@ -51,7 +71,7 @@ def upload_file(ftp):
 
 def manager_files():
     try:
-        os.mkdir(path_backup, 0o775)
+        os.mkdir(args.campaign+path_backup, 0o775)
     except FileExistsError:
         print("The local backup Directory already exists!")
     else:
@@ -64,13 +84,15 @@ def manager_files():
 
 
 # Backup Directory
-path_backup = "backup/"
+timestamp_dir = datetime.datetime.now()
+path_backup = "backup" + "-" + timestamp_dir.strftime("%Y-%m-%d-%H:%M:%S") + "/"
 
 # Args to script file
 parser = argparse.ArgumentParser()
 parser.add_argument("-H", "--host", help="FTP's address")
 parser.add_argument("-u", "--user", help="FTP's user")
 parser.add_argument("-p", "--password", help="FTP's password")
+parser.add_argument("-C", "--campaign", help="FTP's Campaign")
 args = parser.parse_args()
 
 if __name__ == "__main__":
